711fc695975db9c5dcdccf7635adf483429a276d
fakile_john <fakilejohn@gmail.com>
2022-09-08 15:53:00 -0700

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,4 @@
 /**
-<<<<<<< HEAD
-* main
-* Return:0
-*/
-int main(void)
-{
-	puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
-=======
  * main -entry point
  *
  * Return: 0 after printing the function
@@ -17,5 +8,4 @@ int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
->>>>>>> 906f6bb3399cb9b99a7897ed63d98a32f0279acf
 }
