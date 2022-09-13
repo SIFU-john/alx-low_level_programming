d541d182dda307bfc9cdb24952c0d32bd42b94e9
SIFU-john <53531966+SIFU-john@users.noreply.github.com>
2022-09-14 01:53:24 +0300

    Update 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - adds two nums
+*
+* @num: initial num
+* @numm: final num
+*
+* Return: sum of two nums
+*/
+int add(int num, int numm)
+{	
+	return (num + numm);
+}
