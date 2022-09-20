80ff881cbede40a84cf2cb341d953b9c636808d3
fakile_john <fakilejohn@gmail.com>
2022-09-19 17:07:07 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.c"
+/**
+* strlen - returns the length of a string
+* main - check the code
+* @s: string to evaluate
+*
+* Return: Always 0.
+* Return: the length of the string
+*/
+int _strlen(char *s)
+{
+int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+				{
+							i++;
+
+								}
+
+
+
+			return (i);
+}
