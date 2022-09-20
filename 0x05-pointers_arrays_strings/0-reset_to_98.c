f948622cc30d6354b2f3ca125dfa9366f16d66aa
SIFU-john <53531966+SIFU-john@users.noreply.github.com>
2022-09-20 03:20:52 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,11 @@
 #include "main.c"
 /**
-* Reset_to_98 - updates the value of the variable
-*
+* reset_to_98 - updates the value of the variable
 * the pointer points to to 98
-*
 * @n: pointer to the variable to update
-*
-* Return: Always 0.
+*  Return: Always 0.
 */
 void reset_to_98(int *n)
-
 {
 	*n = 98;
 }
