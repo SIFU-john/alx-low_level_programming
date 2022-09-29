ba274a5fddb4fd30febea7adf23a790cc461d344
SIFU-john <53531966+SIFU-john@users.noreply.github.com>
2022-09-29 03:37:50 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,8 @@
-
 #include "main.h"
 /**
 * _puts_recursion - prints a string, followed by a new line
 * @s: string to print
-* Return: Always 0
+* Return: Always 0.
 */
 void _puts_recursion(char *s)
 {
