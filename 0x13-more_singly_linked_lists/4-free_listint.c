6eb8b581a3c3941a8a98f0ba9f2d91a558724dd8
John Adeyinka Fakile <53531966+SIFU-john@users.noreply.github.com>
2022-10-25 19:23:02 +0300

    Commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * Return: Always 0.
+ * @head: listint_t list to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
